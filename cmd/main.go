a08db81bbd37150cc1923721d7245cb2fa5b4d03
IPampurin <I.Pampurin@yandex.ru>
2026-03-10 22:46:22 +0500

    add domain models & server

---
+++ cmd/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/IPampurin/EventBooker/pkg/server"
+	"github.com/wb-go/wbf/logger"
+)
+
+func main() {
+
+	// cоздаём контекст
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// запускаем горутину обработки сигналов
+	go signalHandler(ctx, cancel)
+
+	// считываем .env файл
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		log.Fatalf("Ошибка загрузки конфигурации: %v", err)
+	}
+
+	// настраиваем логгер
+	appLogger, err := logger.InitLogger(
+		logger.ZapEngine,
+		"ImageProcessor",
+		os.Getenv("APP_ENV"), // пока оставим пустым
+		logger.WithLevel(logger.InfoLevel),
+	)
+	if err != nil {
+		log.Fatalf("Ошибка создания логгера: %v", err)
+	}
+	defer func() { _ = appLogger.(*logger.ZapAdapter) }()
+
+	// получаем экземпляр БД
+	storageDB, err := db.InitDB(ctx, &cfg.DB, appLogger)
+	if err != nil {
+		appLogger.Error("ошибка подключения к БД", "error", err)
+		return
+	}
+	defer func() { _ = db.CloseDB(storageDB) }()
+
+	// получаем паблишер RabbitMQ, консумер RabbitMQ, error
+
+	// получаем экземпляр Redis, error (просто реализует положить-отдать для использования методов в сервисном слое)
+
+	// заводим канал для передачи номеров броней (из горутины получения просроченных броней из RabbitMQ в сервисный слой)
+
+	// получаем экземпляр слоя бизнес-логики
+
+	// запускаем горутину получения из Redis просроченных броней (передаём паблишер RabbitMQ)
+
+	// запускаем горутину получения просроченных броней из RabbitMQ (передаём консумер RabbitMQ)
+
+	// запускаем сервер
+	err = server.Run(ctx, &cfg.Server, service, appLogger)
+	if err != nil {
+		appLogger.Error("Ошибка сервера", "error", err)
+		cancel()
+		return
+	}
+
+	appLogger.Info("Приложение корректно завершено")
+}
+
+// signalHandler обрабатывет сигналы отмены
+func signalHandler(ctx context.Context, cancel context.CancelFunc) {
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(sigChan)
+
+	select {
+	case <-ctx.Done():
+		return
+	case <-sigChan:
+		cancel()
+		return
+	}
+}
